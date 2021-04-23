ad0f41d6dbfa8a5b75087a6d4210c41152ea074d
G-MF <gusti.fahrubi@gmail.com>
2021-04-23 11:07:38 +0800

    edit footer

--- template/public/footer.php
+++ template/public/footer.php
@@ -1,5 +1,5 @@
 <footer class="main-footer text-center" style="background-color: #343a40; color: white;">
     <div class="container">
-        <strong>Copyright &copy; 2014-2019 <a href="https://adminlte.io">AdminLTE.io</a>.</strong> All rights reserved.
+        <strong>Copyright &copy; <u>Dinas Kesehatan Provinsi Kalimantan Selatan</u> <?= date('Y') ?>
     </div>
 </footer>
